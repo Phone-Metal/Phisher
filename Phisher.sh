226881be98db1332f5e05ff0123fc6961548f2a0
Phone Metal <74254069+Phone-Metal@users.noreply.github.com>
2021-04-09 10:43:33 +0600

    Update Phisher.sh
    Shortned description

--- Phisher.sh
+++ Phisher.sh
@@ -5,10 +5,10 @@ trap 'printf "\n";stop;exit 1' 2
 
 dependencies() {
 
-command -v php > /dev/null 2>&1 || { echo >&2 "I require php but it's not installed. Install it. Aborting."; exit 1; }
-command -v wget > /dev/null 2>&1 || { echo >&2 "I require wget but it's not installed. Install it. Aborting."; exit 1; }
-command -v unzip > /dev/null 2>&1 || { echo >&2 "I require unzip but it's not installed. Install it. Aborting."; exit 1; }
-command -v curl > /dev/null 2>&1 || { echo >&2 "I require curl but it's not installed. Install it. Aborting."; exit 1; }
+command -v php > /dev/null 2>&1 || { echo >&2 "PHP Not Found"; exit 1; }
+command -v wget > /dev/null 2>&1 || { echo >&2 "WGET Not Found "; exit 1; }
+command -v unzip > /dev/null 2>&1 || { echo >&2 "Unzio Not Found"; exit 1; }
+command -v curl > /dev/null 2>&1 || { echo >&2 "Curl Not Found"; exit 1; }
 
 }
 
